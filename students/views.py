6a12c0a6c46bcc410d713c7847ebdf38170c8388
”Mikhail <mikhail.serdyuk.ms@gmail.com>
2017-09-26 11:59:27 +0300

    static resources

--- students/views.py
+++ students/views.py
@@ -1,6 +1,42 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+#
+from django.template import RequestContext, loader
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# views for Students
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+def students_add(request):
+    return HttpResponse('<h1>Student Add Form</h1>')
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+# Views for Groups
+def groups(request):
+    return HttpResponse('<h1>Groups Listing</h1>')
+def groups_add(request):
+    return HttpResponse('<h1>Groups Add Form</h1>')
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
+
+# Views for Journal
+def journal_list(request):
+    return HttpResponse('<h1>Journal Listing</h1>')
+def journal_student(request, jid):
+    return HttpResponse('<h1>Journal Listing Student %s</h1>' % jid)
+
+
+
+#def test(request):
+#    return render(request, 'students/students_list.html', {})
+#    template = loader.get_template('demo.html')
+#    context = RequestContext(request, {})
+#    return HttpResponse(template.render(context))
