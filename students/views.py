95b8cc4bbcad140f24748daae1e2cc2fa0e44926
”Mikhail <mikhail.serdyuk.ms@gmail.com>
2017-09-28 22:19:00 +0300

    template

--- students/views.py
+++ students/views.py
@@ -9,7 +9,24 @@ from django.http import HttpResponse
 
 # views for Students
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Андрей',
+         'last_name': u'Корост',
+         'ticket': 235,
+         'image': 'img/1.jpeg'},
+        {'id': 2,
+         'first_name': u'Вася',
+         'last_name': u'Пупкин',
+         'ticket': 2123,
+         'image': 'img/2.png'},
+        {'id': 3,
+         'first_name': u'Федя',
+         'last_name': u'Жмурик',
+         'ticket': 345,
+         'image': 'img/3.jpg'}
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
 def students_edit(request, sid):
